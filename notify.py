2791197b365437af9564f2971f0c12ec753321b1
Edu Gómez Escandell <edu.gomez.escandell@canonical.com>
2022-06-29 18:05:13 +0200

    Improved CLI

--- notify.py
+++ notify.py
@@ -2,17 +2,17 @@
 import sys
 import subprocess
 import re
+from typing import List
 
-
-def upgrade_message(upgrade_command: str, *args: list, verbose=False) -> str:
-    full_command = [upgrade_command, *args]
+def upgrade_message(update_query_command: str, *flags: List[str], verbose=False) -> str:
+    full_command = [update_query_command, *flags]
     release_update_response = subprocess.run(full_command, capture_output=True).stdout.decode("utf-8")
     if verbose:
         print("> " + release_update_response.replace("\n", "\n> "))
 
     result = re.search(r"New release '(.*)' available\.", release_update_response)
     if not result:
-        return None
+        return ""
 
     release = result.group(1)
     return "\n".join([
@@ -24,10 +24,38 @@ def upgrade_message(upgrade_command: str, *args: list, verbose=False) -> str:
         "read and modify file /etc/update-manager/release-upgrades",
     ])
 
+def help() -> str:
+    print("Displays a message if there is an Ubuntu update")
+    print("available, does nothing otherwise. It is intended")
+    print("to be called from .bashrc or .profile.")
+    print("Usage: notify.py [FLAG]")
+    print("Flags: ")
+    print("  -v                Enables verbose mode")
+    print("  -h or --help      Prints this screen")
 
-def main():
-    verbose = "-v" in sys.argv
+def parse_arguments() -> bool:
+    argv = set(sys.argv[1:])
 
+    if "-h"  in argv or "--help" in argv:
+        help()
+        sys.exit(0)
+
+    verbose = False
+    if "-v" or "--verbose" in argv:
+        argv.difference_update({"-v", "--verbose"})
+        verbose = True
+
+    if argv:
+        help()
+        print()
+        print(f"Unknown argument{'s' if len(argv) > 1 else ''}: ")
+        [print(f"    {a}") for a in argv]
+        sys.exit(1)
+
+    return verbose
+
+def main() -> int:
+    verbose = parse_arguments()
     try:
         msg = upgrade_message("do-release-upgrade", "-c", verbose=verbose)
         if msg:
