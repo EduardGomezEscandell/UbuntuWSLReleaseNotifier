e295036c5b1704ad0c3594698ea03a128edf4c3a
Edu Gómez Escandell <edu.gomez.escandell@canonical.com>
2022-06-30 13:22:45 +0200

    Simplified timing

--- notify.py
+++ notify.py
@@ -10,7 +10,7 @@ import json
 from typing import List, Tuple, Dict
 
 
-_CONFIG_FILE = os.path.join(os.path.dirname(__file__), "config.json")
+_TIMESTAMP_FILE = os.path.join(os.path.dirname(__file__), "last_notification_timestamp")
 
 
 def _log(enable: bool, *message) -> None:
@@ -31,6 +31,35 @@ def _frequecies() -> Dict[str, int]:
     return freqs
 
 
+def _read_timestamp(verbose: bool) -> datetime.datetime:
+    _log(verbose, f"Reading file {_TIMESTAMP_FILE}")
+    try:
+        with open(_TIMESTAMP_FILE, "r") as f:
+            return datetime.datetime.fromisoformat(f.read())
+    except Exception as e:
+        _log(verbose, "Failed to read timestamp")
+        return datetime.datetime(1900, 1, 1).astimezone()
+
+
+def _write_timestamp(timestamp: datetime.datetime, verbose: bool) -> None:
+    _log(verbose, f"Updating file {_TIMESTAMP_FILE}")
+    try:
+        with open(_TIMESTAMP_FILE, "w") as f:
+            f.write(timestamp.isoformat())
+    except Exception as e:
+        _log(verbose, "Failed to write timestamp")
+
+
+def _print_or_raise(errcode: int, message_or_exception, verbose: bool) -> int:
+    if not errcode and message_or_exception:
+        _log(True, message_or_exception)
+    elif not errcode:
+        _log(verbose, "NO MESSAGE")
+    elif errcode and verbose:
+        raise message_or_exception
+    return errcode
+
+
 def _parse_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         description='Displays a message if there is an Ubuntu update available, does nothing otherwise.')
@@ -41,93 +70,19 @@ def _parse_arguments() -> argparse.Namespace:
         '-t', '--timeout', dest='timeout', type=float, metavar='SECONDS', default=1.5,
         help='set a timeout for the upgrade querry.')
     parser.add_argument(
-        '-f', '--set-frequency', dest='frequency', type=str, choices=_frequecies().keys(),
-        help="set the frequency at which you are notified"
+        '-f', '--frequency', dest='frequency', type=str, choices=_frequecies().keys(),
+        default="daily", help="set the frequency at which you are notified"
     )
 
     return parser.parse_args()
 
 
-def _read_config(verbose: bool) -> str:
-    _log(verbose, f"Reading file {_CONFIG_FILE}")
-    with open(_CONFIG_FILE, "r") as f:
-        text = f.read()
-        if text:
-            return json.loads(text)
-        else:
-            return None
-
-
-def _write_config(config: dict, verbose: bool) -> None:
-    _log(verbose, f"Updating file {_CONFIG_FILE}")
-    with open(_CONFIG_FILE, "w") as f:
-        f.write(json.dumps(config, indent=2))
-
-
-def _set_frequency(frequency: str, verbose: bool = False) -> int:
-    if frequency not in _frequecies():
-        _log(verbose, f"Frequency '{frequency}' unknown. Use --help to see the options.")
-        return 1
-
-    config = {}
-    try:
-        config = _read_config(verbose)
-    except FileNotFoundError:
-        _log(verbose, f"File {_CONFIG_FILE} not found, creating it anew.")
-    except Exception:
-        _log(verbose, f"File {_CONFIG_FILE} invalid, creating it anew.")
-
-    if "last_notif" not in config:
-        config["last_notif"] = "1900-01-01T00:00:00+00:00"
-    config["notification_frequency"] = frequency
-
-    try:
-        _write_config(config, verbose)
-        return 0
-    except Exception as e:
-        return (1, e)
-
-
-def _check_frequency(verbose: bool) -> Tuple[int, bool]:
-    _log(verbose, f"Reading file {_CONFIG_FILE}")
-
+def _check_frequency(frequency: str, verbose: bool) -> bool:
+    last_notif = _read_timestamp(verbose)
+    validator = _frequecies()[frequency]
     now = datetime.datetime.now().astimezone()
-
-    try:
-        config = _read_config(verbose)
-    except Exception as e:
-        return (1, e)
-
-    if "notification_frequency" not in config:
-        config["notification_frequency"] = "daily"
-
-    if "last_notif" not in config:
-        config["last_notif"] = now.isoformat()
-
-    last_notif = datetime.datetime.fromisoformat(config["last_notif"])
-    validator = _frequecies()[config["notification_frequency"]]
-
-    must_notify = validator(now, last_notif)
-
-    if must_notify:
-        config["last_notif"] = now.isoformat()
-
-    try:
-        _write_config(config, verbose)
-    except Exception as e:
-        return (1, e)
-
-    return (0, must_notify)
-
-
-def print_or_raise(errcode: int, message_or_exception, verbose: bool) -> int:
-    if not errcode and message_or_exception:
-        _log(True, message_or_exception)
-    elif not errcode:
-        _log(verbose, "NO MESSAGE")
-    elif errcode and verbose:
-        raise message_or_exception
-    return errcode
+    _write_timestamp(now, verbose)
+    return validator(now, last_notif)
 
 
 def upgrade_message(update_query_cmd: str, *flags: List[str], verbose: bool = False, timeout: float = 5) -> Tuple[int, str]:
@@ -158,16 +113,11 @@ def upgrade_message(update_query_cmd: str, *flags: List[str], verbose: bool = Fa
 def main() -> int:
     args = _parse_arguments()
 
-    if args.frequency:
-        return _set_frequency(args.frequency, args.verbose)
-
-    errcode, must_run = _check_frequency(args.verbose)
-
-    if not must_run:
-        return print_or_raise(errcode, must_run, args.verbose)
+    if not _check_frequency(args.frequency, args.verbose):
+        return 0
 
     errcode, msg = upgrade_message("do-release-upgrade", "-c", verbose=args.verbose, timeout=args.timeout)
-    return print_or_raise(errcode, msg, args.verbose)
+    return _print_or_raise(errcode, msg, args.verbose)
 
 
 if __name__ == '__main__':
