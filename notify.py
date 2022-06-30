3539e6c9309bfb875ae307ad1c3b89ea5be999c1
Edu Gómez Escandell <edu.gomez.escandell@canonical.com>
2022-06-30 10:23:21 +0200

    Refactor to make failure testable

--- notify.py
+++ notify.py
@@ -2,28 +2,35 @@
 import sys
 import subprocess
 import re
-from typing import List
+from typing import List, Tuple
 
 
-def upgrade_message(update_query_command: str, *flags: List[str], verbose=False) -> str:
+def upgrade_message(update_query_command: str, *flags: List[str], verbose=False) -> Tuple[int, str]:
     full_command = [update_query_command, *flags]
-    release_update_response = subprocess.run(full_command, capture_output=True).stdout.decode("utf-8")
+    
+    try:
+        release_update_response = subprocess.check_output(full_command, text=True, stderr=subprocess.STDOUT, encoding="utf-8")
+    except Exception as e:
+        if verbose:
+            raise e
+        return (1, e)
+
     if verbose:
         print("> " + release_update_response.replace("\n", "\n> "))
 
     result = re.search(r"New release '(.*)' available\.", release_update_response)
     if not result:
-        return ""
+        return (0, "")
 
     release = result.group(1)
-    return "\n".join([
-        "Release available",
-        "=================",
-        f"A new release of Ubuntu is available: Ubuntu {release}",
-        "To know more, run do-release-upgrade",
-        "To disable or change the frequency of these alerts,",
-        "read and modify file /etc/update-manager/release-upgrades",
-    ])
+    return (0,
+        "Release available\n"                                             \
+        "=================\n"                                             \
+        f"A new release of Ubuntu is available: Ubuntu {release}\n"       \
+        "To know more, run do-release-upgrade\n"                          \
+        "To disable or change the frequency of these alerts,\n"           \
+        "read and modify file /etc/update-manager/release-upgrades"
+    )
 
 
 def _help() -> str:
@@ -60,17 +67,17 @@ def _parse_arguments() -> bool:
 
 def main() -> int:
     verbose = _parse_arguments()
-    try:
-        msg = upgrade_message("do-release-upgrade", "-c", verbose=verbose)
-        if msg:
-            print(msg)
-        elif verbose:
-            print("NO MESSAGE")
-        return 0
-    except Exception as e:
-        if verbose:
-            raise e
-        return 1
+
+    errcode, msg = upgrade_message("do-release-upgrade", "-c", verbose=verbose)
+    
+    if not errcode and msg:
+        print(msg)
+    elif not errcode and verbose:
+        print("NO MESSAGE")
+    elif errcode and verbose:
+        raise msg
+
+    return errcode
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -7,13 +7,14 @@ class TestUpgradeNotifier(unittest.TestCase):
         fake_response = r"\"Checking for a new Ubuntu release\n"                            \
                         r"New release '21.10' available.\n"                                 \
                         r"Run 'do-release-upgrade' to upgrade to it.'\""
-        msg = notify.upgrade_message("printf", fake_response)
+        errcode, msg = notify.upgrade_message("printf", fake_response)
         expected = "Release available\n"                                                    \
                    "=================\n"                                                    \
                    "A new release of Ubuntu is available: Ubuntu 21.10\n"                   \
                    "To know more, run do-release-upgrade\n"                                 \
                    "To disable or change the frequency of these alerts,\n"                  \
                    "read and modify file /etc/update-manager/release-upgrades"
+        self.assertEqual(errcode, 0)
         self.assertEqual(msg, expected)
 
     def testUnAvailableUpgrade(self):
@@ -21,28 +22,22 @@ class TestUpgradeNotifier(unittest.TestCase):
                         r"There is no development version of an LTS available.\n"           \
                         r"To upgrade to the latest non-LTS development release \n"          \
                         r"set Prompt=normal in /etc/update-manager/release-upgrades.'\""
-        msg = notify.upgrade_message("printf", fake_response)
+        errcode, msg = notify.upgrade_message("printf", fake_response)
+        self.assertEqual(errcode, 0)
         self.assertFalse(msg)
 
     def testDisabledUpgrade(self):
         fake_response = r"\"Checking for a new Ubuntu release\n"                            \
                         r"In /etc/update-manager/release-upgrades Prompt \n"                \
                         r"is set to never so upgrading is not possible.\""
-        msg = notify.upgrade_message("printf", fake_response)
+        errcode, msg = notify.upgrade_message("printf", fake_response)
+        self.assertEqual(errcode, 0)
         self.assertFalse(msg)
 
     def testFailure(self):
-        fake_response = r"\"Checking for a new Ubuntu release\n"                            \
-                        r"^CTraceback (most recent call last):\n"                           \
-                        r"  File \"/usr/bin/do-release-upgrade\", line 136, in <module>\n"  \
-                        r"    m.downloaded.wait()\n"                                        \
-                        r"  File \"/usr/lib/python3.8/threading.py\", line 558, in wait\n"  \
-                        r"    signaled = self._cond.wait(timeout)\n"                        \
-                        r"  File \"/usr/lib/python3.8/threading.py\", line 302, in wait\n"  \
-                        r"    waiter.acquire()\n"                                           \
-                        r"KeyboardInterrupt\""
-        msg = notify.upgrade_message("printf", fake_response)
-        self.assertFalse(msg)
+        errcode, msg = notify.upgrade_message("python3", "-c", "raise RuntimeError('test')")
+        self.assertEqual(errcode, 1)
+        self.assertIsInstance(msg, Exception)
 
 
 if __name__ == '__main__':
